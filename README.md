fea7d2985ef4f2edf2f0f998550fc02244e5888a
Hugibeer <hugibeer@gmail.com>
2020-04-24 11:32:51 +0200

    Added configurability via command line parameters. Also, implemented SQL procedure tests. Implemented ability to run multiple threads of the measurements

--- README.md
+++ README.md
@@ -18,15 +18,27 @@ CREATE TABLE dbo.Test
 	Name NVARCHAR(100) NOT NULL,
 	DateOfBirth DATE NOT NULL DEFAULT '19991231'
 )
+
+CREATE TYPE [dbo].[TestInsertType] AS TABLE(
+	Name NVARCHAR(100) NOT NULL,
+	DateOfBirth DATE NOT NULL
+)
+
+CREATE PROCEDURE [dbo].[InsertTest]
+    @myTableType TestInsertType readonly
+AS
+BEGIN
+    insert into [dbo].Test select * from @myTableType 
+END
 ```
 3. Modify `app.config` file section
-```
+```XML
 <add name="Context"
     connectionString="Data Source=<yourDbServerName>;Initial Catalog=SampleDatabase;Integrated Security=SSPI;MultipleActiveResultSets=True"
     providerName="System.Data.SqlClient" />
-
 ```
-4. Open sln file and run it
+4. Open command prompty and run `dotnet build`
+5. Navigate to TannerydSample\ConsoleApp1\bin\Debug and run `.\ConsoleApp1.exe 1 proc`. This will measure thread execution with sql procedure insert. To use Tanneryd bulk operations, replace `proc` with `bulk`
 
 ## Remarks
 Solution is created in Visual Studio 2019 community edition.
--- TannerydSample/ConsoleApp1/Program.cs
+++ TannerydSample/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using Tanneryd.BulkOperations.EF6;
 
 namespace ConsoleApp1
@@ -13,10 +16,41 @@ namespace ConsoleApp1
 
 		static void Main(string[] args)
 		{
-			var data = GenerateTestData();
-
 			DeleteOldEntries();
+			if (args.Length != 2)
+			{
+				Console.WriteLine("Use this tool with two parameters:");
+				Console.WriteLine("\trange should be integer for number of threads to execute");
+				Console.WriteLine("\tproc/bulk use proc to execute sql procedure, use bulk to execute tannyrd bulk insert all operation");
+				Console.WriteLine("Sample: consoleapp1.exe 3 proc");
+				return;
+			}
 
+			var range = int.Parse(args[0]);
+			var useSql = args[1].ToLower() == "proc";
+
+			Task[] taskArray = new Task[range];
+			for (int i = 0; i < taskArray.Length; i++)
+			{
+				taskArray[i] = Task.Factory.StartNew(() => Measure(useSql));
+			}
+			Task.WaitAll(taskArray);
+		}
+
+		private static Double DoComputation(Double start)
+		{
+			var data = GenerateTestData();
+			Double sum = 0;
+			for (var value = start; value <= start + 10; value += .1)
+				sum += value;
+
+			return sum;
+		}
+
+		private static void Measure(bool useProcedure)
+		{
+			//Console.WriteLine($"Started at {DateTime.Now}");
+			var data = GenerateTestData();
 			var measurements = new List<double>();
 			var stopwatch = new Stopwatch();
 
@@ -26,12 +60,21 @@ namespace ConsoleApp1
 				foreach (var bulk in data.BulkTake(CHUNK_SIZE))
 				{
 					var chunkWatch = new Stopwatch();
-
 					chunkWatch.Start();
-					context.BulkInsertAll(bulk.ToList());
+
+					if (useProcedure)
+					{
+						var parameter = GenerateDataTable("parameterName", bulk);
+						var rowsAffected = context.Database.ExecuteSqlCommand("exec dbo.InsertTest @parameterName", parameter);
+					}
+					else
+					{
+						context.BulkInsertAll(bulk.ToList());
+					}
+
 					chunkWatch.Stop();
 
-					Console.WriteLine($"Elapsed {chunkWatch.Elapsed.TotalSeconds}");
+					//Console.WriteLine($"Elapsed {chunkWatch.Elapsed.TotalSeconds}");
 
 					measurements.Add(chunkWatch.Elapsed.TotalSeconds);
 				}
@@ -39,14 +82,41 @@ namespace ConsoleApp1
 				stopwatch.Stop();
 			}
 			var average = measurements.Average();
-			Console.WriteLine($"Total elapsed time {stopwatch.Elapsed.TotalSeconds}, average seconds per {CHUNK_SIZE} entries {average}");
+			var elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+			Console.WriteLine($"Total elapsed time {elapsedSeconds}, average seconds per {CHUNK_SIZE} entries {average}");
+
+			//Console.WriteLine($"Ended at {DateTime.Now}");
+		}
+
+		private static SqlParameter GenerateDataTable(string parameterName, List<Test> bulk)
+		{
+			var dt = new DataTable();
+			dt.Columns.Add("Name", typeof(string));
+			dt.Columns.Add("DateOfBirth", typeof(DateTime));
+
+			foreach (var value in bulk)
+			{
+				var row = dt.NewRow();
+				row[0] = value.Name;
+				row[1] = value.DateOfBirth;
+				dt.Rows.Add(row);
+			}
+			//return dt;
+
+			var sqlParameter = new SqlParameter(parameterName, SqlDbType.Structured)
+			{
+				TypeName = "dbo.TestInsertType",
+				Value = dt
+			};
+
+			return sqlParameter;
 		}
 
 		private static void DeleteOldEntries()
 		{
 			using (var context = new Context())
 			{
-				context.Database.ExecuteSqlCommand("DELETE FROM Test");
+				context.Database.ExecuteSqlCommand("truncate table Test");
 			}
 		}
 
